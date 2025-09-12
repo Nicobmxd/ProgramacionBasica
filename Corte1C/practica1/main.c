f1202177dbacd80bb2d3cba8979d3653b6e15aac
Nicobmxd <nicolasbenavidesmoreno@gmail.com>
2025-09-12 13:31:05 -0500

    Add files via upload

---
+++ Corte1C/practica1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ run this program using the console pauser or add your own getch, system("pause") or input loop
+ Hola estoy practicando programación 
+
+*/
+
+//quiero aprender a programas en c
+
+int main(int argc, char *argv[]) {
+	
+	printf("Hola mundo");
+	return 0;
+}
